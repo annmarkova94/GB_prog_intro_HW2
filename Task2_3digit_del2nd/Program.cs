f058cc81bf4a233590cf9e6de15c154ef7e29a75
annmark94 <annmarkova94@gmail.com>
2022-06-27 23:04:22 +0300

    task2 - initial commit, added a folder about generating a random 3-digit number and deleting the 2nd digit

---
+++ Task2_3digit_del2nd/Program.cs
@@ -0,0 +1,4 @@
+﻿int numberX = new Random().Next(100, 1000);
+Console.WriteLine($"The random 3-digit number: {numberX}");
+
+Console.WriteLine($"This number if we delete the 2nd digit: {numberX / 100 * 10 + numberX % 10}");
