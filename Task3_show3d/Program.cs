e1af00bb53505a842c7c698d9a68d70359378ed8
annmark94 <annmarkova94@gmail.com>
2022-06-27 23:32:24 +0300

    task3 - initial commit, added a folder about showing the 3rd digit

---
+++ Task3_show3d/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Write your number: ");
+int numberX = int.Parse(Console.ReadLine());
+string numberX_str = Convert.ToString(numberX);
+
+if (numberX_str.Length < 3) Console.Write("This number does not have the 3d digit");
+else Console.Write(numberX_str[2]);
