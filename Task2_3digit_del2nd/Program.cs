d0a48354669ba5c15eec55c3abdcde298f5a1c51
annmark94 <annmarkova94@gmail.com>
2022-06-29 23:04:11 +0300

    task2 - added description in the beginning

--- Task2_3digit_del2nd/Program.cs
+++ Task2_3digit_del2nd/Program.cs
@@ -1,4 +1,5 @@
-﻿int numberX = new Random().Next(100, 1000);
+﻿//Generate a 3-digit random number, delete the second digit and print out
+int numberX = new Random().Next(100, 1000);
 Console.WriteLine($"The random 3-digit number: {numberX}");
 
 Console.WriteLine($"This number if we delete the 2nd digit: {numberX / 100 * 10 + numberX % 10}");
