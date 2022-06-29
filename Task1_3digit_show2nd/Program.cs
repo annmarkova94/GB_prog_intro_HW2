03cf00adfb11cc1affa04875f0e3d70fbc8d1911
annmark94 <annmarkova94@gmail.com>
2022-06-29 22:52:10 +0300

    task1 - added a while loop to catch situations when number of digits does not equal 3

--- Task1_3digit_show2nd/Program.cs
+++ Task1_3digit_show2nd/Program.cs
@@ -1,4 +1,8 @@
 ﻿Console.Write("Write a 3-digit number: ");
 int numberX = int.Parse(Console.ReadLine());
-
-Console.Write($"Second digit is: {numberX % 100 / 10}");
+while (numberX < 100 || numberX > 999)
+{
+    Console.Write("Number of digits does not equal 3, try again: ");
+    numberX = int.Parse(Console.ReadLine());
+}
+Console.Write($"The second digit of your 3-digit number {numberX} is {numberX % 100 / 10}");
