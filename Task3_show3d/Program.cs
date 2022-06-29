4a4cf26fb9a757fec2818fd19b6b101bd9e60024
annmark94 <annmarkova94@gmail.com>
2022-06-29 23:08:48 +0300

    task3 - added description in the beginning and adjusted wording

--- Task3_show3d/Program.cs
+++ Task3_show3d/Program.cs
@@ -1,6 +1,7 @@
-﻿Console.Write("Write your number: ");
+﻿//Take a number and show it's 3rd digit or say that there in no 3rd digit
+Console.Write("Write your number: ");
 int numberX = int.Parse(Console.ReadLine());
 string numberX_str = Convert.ToString(numberX);
 
 if (numberX_str.Length < 3) Console.Write("This number does not have the 3d digit");
-else Console.Write(numberX_str[2]);
+else Console.Write($"The 3rd digit of your number is {numberX_str[2]}");
